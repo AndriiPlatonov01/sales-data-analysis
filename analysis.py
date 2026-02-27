65656280a3b887fb8e65f435d62965d0156d18ec
AndriiPlatonov01! <aplatonov01@gmail.com>
2026-02-27 19:06:49 +0100

    added analysis file

---
+++ analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Дані про продажі по містах
+data = {
+    'City': ['Kyiv', 'Lviv', 'Odesa', 'Kyiv', 'Lviv', 'Kharkiv'],
+    'Sales': [1500, 1200, 900, 2100, 1300, 1100]
+}
+
+df = pd.DataFrame(data)
+
+# Розрахунок загальних продажів по містах
+city_sales = df.groupby('City')['Sales'].sum().reset_index()
+
+print("Звіт по продажах по містах:")
+print(city_sales)
